3823d6a9566bb08975a78f9e58ae4d2260b2f64e
Joshua Akinola <46546654+Joshua77@users.noreply.github.com>
2022-07-15 02:03:40 +0000

    Challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sumofArray = 0;
+  for(i = 0; i < arrayOfNumbers.length; i++){
+    sumofArray += arrayOfNumbers[i];
+  }
+    
+  return sumofArray;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let countEvenNum = 0;
+  for(let i = 0; i < arrayOfNumbers.length; i++){
+    if(arrayOfNumbers[i]%2 === 0){
+      countEvenNum++;
+    }
+  }
+  return countEvenNum;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
